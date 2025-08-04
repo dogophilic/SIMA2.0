de718546cfc8d1d1e314f39f714ff62932b17afd
dogophilic <12323280.subhekshana.com>
2025-08-04 17:45:38 +0530

    Updated build.sh to use non-root wkhtmltopdf install

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Create bin directory if not present
+mkdir -p bin
 
-# Install wkhtmltopdf manually from local .deb file
-apt-get update
-apt-get install -y xfonts-75dpi xfonts-base
-dpkg -i ./bin/wkhtmltox_0.12.6-1.bionic_amd64.deb
+# Download precompiled wkhtmltopdf binary
+curl -L https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6/wkhtmltox-0.12.6-1.bionic_amd64.deb -o wkhtmltox.deb
+
+# Extract only the binary without root access
+ar x wkhtmltox.deb
+tar -xJf data.tar.xz
+
+# Move wkhtmltopdf binary to bin/ so we can call it
+mv usr/local/bin/wkhtmltopdf bin/
+chmod +x bin/wkhtmltopdf
+
+# Cleanup
+rm -rf usr/ wkhtmltox.deb control.tar.gz data.tar.xz debian-binary
